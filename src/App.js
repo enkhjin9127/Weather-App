4773cc01fa0c2539e427b750db8623ed48413266
enkhjin9127 <ka0enkhjin@gmail.com>
2025-01-20 11:08:48 +0800

    final commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import searchIcon from "./img/Search-Icon.png";
 import moonIcon from "./img/moon.png";
 import sunIcon from "./img/sun.png";
 import localIcon from "./img/localization_icon.png";
+import homeIcon from "./img/Home.png";
+import heartIcon from "./img/Heart.png";
+import pinIcon from "./img/Pin.png";
+import userIcon from "./img/User.png";
+import homeNightIcon from "./img/homeNight.png";
+import heartNightIcon from "./img/heartNight.png";
+import pinNightIcon from "./img/pinNight.png";
+import userNightIcon from "./img/userNight.png";
+import vectorLeft from "./img/vectorLeft.png";
+import vectorRight from "./img/vectorRight.png";
 
 const weatherApiKey = "1ce21f8398014abf9da21949251501";
 
@@ -107,18 +117,24 @@ const App = () => {
 
   return (
     <div className="flex h-screen">
+      <section>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-300 rounded-full w-[140px] h-[140px] "></div>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-300 rounded-full w-[340px] h-[340px] "></div>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-300 rounded-full w-[540px] h-[540px] "></div>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-300 rounded-full w-[940px] h-[940px] "></div>
+      </section>
       <section className="flex-1 flex flex-col items-center bg-gray-100">
-        <div className="mt-[40px]">
+        <div className="mt-[40px] z-10">
           <div className="relative flex items-center w-[567px] h-[80px] bg-white shadow-md rounded-full right-[70px]">
             <img
-              className="absolute left-6 h-[35px] w-[35px] text-gray-500"
+              className="absolute left-7 h-[35px] w-[35px] text-gray-500"
               src={searchIcon}
               alt="Search-Logo"
             />
             <input
               type="text"
               placeholder="Search"
-              className="w-full py-3 pl-20 pr-6 outline-none text-[32px] font-medium text-gray-700 rounded-full focus:ring-2 focus:ring-orange-400"
+              className="w-full py-3 pl-20 pr-6 outline-none text-[32px] font-bold text-gray-700 rounded-full focus:ring-2 focus:ring-orange-400"
               value={countriesSearch}
               onChange={handleChange}
             />
@@ -149,7 +165,7 @@ const App = () => {
           </div>
         </div>
         {/* Day */}
-        <div className="w-[414px] h-[824px] p-4 rounded-[48px] bg-white bg-opacity-75">
+        <div className="w-[414px] h-[824px] p-4 rounded-[48px] bg-white bg-opacity-75 z-9 backdrop-blur-md">
           {!weatherLoading && !error && (
             <div className="w-full h-full ">
               <div className="py-10 px-8 flex flex-col">
@@ -174,15 +190,21 @@ const App = () => {
                   />
                 </div>
               </div>
-              <div className="w-[414px] h-[269px] pl-9">
+              <div className="w-[382px] h-[269px] px-6">
                 <div className="text-transparent bg-clip-text font-extrabold text-[110px] -mt-10 bg-gradient-to-b from-black to-white">
-                  {weather.max_c}°C
+                  {weather.max_c}°
                 </div>
                 <div
                   className="font-bold mb-12 h-6 font-manrope"
                   style={{ color: "rgb(119, 124, 206)" }}
                 >
                   {weather.condition}
+                </div>
+                <div className="flex justify-between">
+                  <img src={homeIcon} alt="homeIcon" />
+                  <img src={pinIcon} alt="pinIcon" />
+                  <img src={heartIcon} alt="heartIcon" />
+                  <img src={userIcon} alt="userIcon" />
                 </div>
               </div>
             </div>
@@ -192,25 +214,49 @@ const App = () => {
 
       {/* Night */}
       <section
-        className={`flex-1 flex flex-col items-center justify-center bg-customDark text-white`}
+        className={
+          "flex-1 flex flex-col items-center justify-center bg-customDark text-white"
+        }
       >
-        <div className="w-[414px] h-[824px] bg-customDarkAlpha rounded-[48px]">
+        <div className="w-[414px] h-[824px] p-4 rounded-[48px] bg-customDarkAlpha bg-opacity-75 z-9 backdrop-blur-md">
           {!weatherLoading && !error && (
-            <div>
-              <div>
-                <div>
-                  <p>{weather.date}</p>
-                  <p className="text-4xl font-bold mb-4">{selectedCity}</p>
+            <div className="w-full h-full ">
+              <div className="py-10 px-8 flex flex-col">
+                <div className="flex justify-between items-center">
+                  <div>
+                    <h4 className=" text-gray-400 ">{weather.date}</h4>
+                    <h2 className="h-12 text-5xl font-bold mt-2">
+                      {selectedCity}
+                    </h2>
+                  </div>
+                  <img
+                    className="w-[40px] h-[40px]"
+                    src={localIcon}
+                    alt="localization-icon"
+                  />
                 </div>
-                <img src={localIcon} alt="localization-icon" />
+                <div className="mt-12 flex justify-center items-center">
+                  <img
+                    className="w-[262.11px] h-[262.11px]"
+                    src={moonIcon}
+                    alt="moon"
+                  />
+                </div>
               </div>
-              <img
-                className="w-[262.11px] h-[262.11px]"
-                src={moonIcon}
-                alt="moon"
-              />
-              <div>{weather.min_c}°C</div>
-              <div>{weather.condition}</div>
+              <div className="w-[382px] h-[269px] px-6">
+                <div className="text-transparent bg-clip-text font-extrabold text-[110px] -mt-10 bg-gradient-to-b from-black to-white">
+                  {weather.min_c}°
+                </div>
+                <div className="font-bold mb-12 h-6 font-manrope text-orange-400">
+                  {weather.condition}
+                </div>
+                <div className="flex justify-between">
+                  <img src={homeNightIcon} alt="homeIcon" />
+                  <img src={pinNightIcon} alt="pinIcon" />
+                  <img src={heartNightIcon} alt="heartIcon" />
+                  <img src={userNightIcon} alt="userIcon" />
+                </div>
+              </div>
             </div>
           )}
         </div>
